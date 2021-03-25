83073e8977247774d6d8e4ef3ea80c7982efcdfc
Laurent Grimaldi <laurentgrim.95@outlook.com>
2021-03-26 00:41:37 +0100

    WIP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -135,7 +135,7 @@
           style="color: black"
         >
           <v-card-title class="text-h5 font-weight-bold">
-            Vous voulez en s'avoir plus ? ? ? -- ? ? ?
+            Vous voulez en s'avoir plus ?
           </v-card-title>
           <v-card-text class="text-h6 font-weight-regular" style="color: black">
             N'hésiter pas à me contacter par email
